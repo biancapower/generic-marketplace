a27b15f59223df8e7ad40cba498564a2e394b17f
biancapower <bianca@biancapower.com>
2022-06-04 09:49:15 +1000

    add condition enum to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,6 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {well_used: 1, barely_used: 2, almost_new: 3, brand_new: 4}
 end
