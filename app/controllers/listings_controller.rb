0054e7e5f99796ad13918e2eeb8d5ebccd8356ca
biancapower <bianca@biancapower.com>
2022-06-04 10:13:34 +1000

    Implement user authorization (a user can only edit and delete their own products)

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: %i[ show edit update destroy ]
   before_action :set_form_vars, only: [:new, :edit]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
+
 
   # GET /listings or /listings.json
   def index
@@ -68,6 +70,13 @@ class ListingsController < ApplicationController
     def set_form_vars
       @categories = Category.all
   		@conditions = Listing.conditions.keys
+    end
+
+    def authorize_user 
+      if @listing.user_id != current_user.id
+        flash[:alert] = "You don't have permission to do that"
+        redirect_to listings_path
+      end 
     end
 
     # Only allow a list of trusted parameters through.
