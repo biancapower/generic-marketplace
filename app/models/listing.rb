ff987986a3a9d845feb0139ffa4d99e083b5841e
biancapower <bianca@biancapower.com>
2022-06-04 10:39:34 +1000

    implement local image uploading

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+  has_one_attached :picture
 
   enum condition: {well_used: 1, barely_used: 2, almost_new: 3, brand_new: 4}
 end
