01c8abfdaa8bf31ad9bea0f44553d16d238443c9
biancapower <bianca@biancapower.com>
2022-06-05 08:43:25 +1000

    implement super basic purchase of item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :listings
+
+  post "listings/:id/order", to: "listings#place_order", as: "place_order"
+
   devise_for :users
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
