bb07ee33fbc9411be5b9af0080253bfc0fe1d77e
biancapower <bianca@biancapower.com>
2022-06-04 10:00:14 +1000

    set up categories dropdown and condition radio buttons

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,7 @@
 module ListingsHelper
+	def format_condition(condition)
+		# convert symbol_formatting_like_this to string
+		# capitalizes each word
+		condition.split("_").map {|word| word.capitalize}.join(" ")
+	end
 end
