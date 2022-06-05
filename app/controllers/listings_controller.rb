fe2c48368d7ba528a6fd2691ed82a8b3b85768f0
biancapower <bianca@biancapower.com>
2022-06-05 15:16:58 +1000

    sort by clicking on column name

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,7 +7,7 @@ class ListingsController < ApplicationController
 
   # GET /listings or /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.order(params[:sort])
   end
 
   # GET /listings/1 or /listings/1.json
