75494de788768fde007eb3126c056764400c06f0
biancapower <bianca@biancapower.com>
2022-06-05 13:32:08 +1000

    rails g controller Orders success bought sold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'orders/success'
+  get 'orders/bought'
+  get 'orders/sold'
   resources :listings
 
   post "listings/:id/order", to: "listings#place_order", as: "place_order"
